5bbe945038d56f0485dd675ea75834abe6aa497b
Nick Ng <ngyewch@gmail.com>
2025-10-28 17:59:36 +0800

    fix: opener

--- periph/opener.go
+++ periph/opener.go
@@ -1,25 +1,30 @@
 package periph
 
 import (
-	"fmt"
 	"time"
 
-	"github.com/ngyewch/coptonix-rs232-i2c"
+	coptonixrs232i2c "github.com/ngyewch/coptonix-rs232-i2c"
 	"go.bug.st/serial"
 	"periph.io/x/conn/v3/i2c"
 	"periph.io/x/conn/v3/i2c/i2creg"
 )
 
+var (
+	busMap = make(map[string]i2c.BusCloser, 0)
+)
+
 func NewOpener(name string, portName string, mode *serial.Mode, readTimeout time.Duration) i2creg.Opener {
-	fmt.Println("NewOpener")
 	return func() (i2c.BusCloser, error) {
-		fmt.Printf("coptonix-rs232-i2c opening %s %s\n", name, portName)
-		serialPort, err := coptonixrs232i2c.OpenSerialPort(portName, mode, readTimeout)
-		if err != nil {
-			return nil, err
+		bus, ok := busMap[name]
+		if !ok {
+			serialPort, err := coptonixrs232i2c.OpenSerialPort(portName, mode, readTimeout)
+			if err != nil {
+				return nil, err
+			}
+			dev := coptonixrs232i2c.New(serialPort)
+			bus = NewBus(name, dev)
+			busMap[name] = bus
 		}
-		dev := coptonixrs232i2c.New(serialPort)
-		bus := NewBus(name, dev)
 		return bus, nil
 	}
 }
--- periph/register.go
+++ periph/register.go
@@ -1,7 +1,6 @@
 package periph
 
 import (
-	"fmt"
 	"time"
 
 	"go.bug.st/serial"
@@ -9,6 +8,5 @@ import (
 )
 
 func Register(name string, aliases []string, portName string, mode *serial.Mode, readTimeout time.Duration) error {
-	fmt.Println("Register")
 	return i2creg.Register(name, aliases, -1, NewOpener(name, portName, mode, readTimeout))
 }
